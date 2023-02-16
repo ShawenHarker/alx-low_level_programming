ef534e31a9fa858484c00b17bd9b9bfc2068ab81
ShawenHarker <harkershawen@gmail.com>
2023-02-16 12:06:49 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Entry point
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -10,4 +10,5 @@ int main(void)
 	char string[] = "\"Programming is like building a multilingual puzzle";
 	puts(string);
 	return (0);
+
 }
