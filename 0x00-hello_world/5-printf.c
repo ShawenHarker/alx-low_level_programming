fbaf8ba158ca20df3d1a202240ababaf3bf77fd9
ShawenHarker <harkershawen@gmail.com>
2023-02-16 12:16:13 -0800

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdin.h>
+
+/**
+ *
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char string[] = "with proper grammar, but the outcome is a piece of art,"
+
+	printf("%d", string);
+	return (0);
+}
