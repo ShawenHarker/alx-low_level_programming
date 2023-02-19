5f0ab854793f0f363e53b3d90bf216583561ad03
ShawenHarker <harkershawen@gmail.com>
2023-02-19 20:21:49 +0200

    Task 100

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		putchar( ch);
+		if (ch < '9')
+		{
+			putchar(",\ ");
+		}
+		if (ch == 9)
+		{
+			putchar("$");
+		}
+	}
+	putchar('\n');
+	return (0);
+}
