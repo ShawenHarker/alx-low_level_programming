350125c0826adad77f0de02005a6e56dfdca1b93
ShawenHarker <harkershawen@gmail.com>
2023-02-21 19:22:14 +0200

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Printing the alphabet
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	_putchar('\n');
+	return;
+}
