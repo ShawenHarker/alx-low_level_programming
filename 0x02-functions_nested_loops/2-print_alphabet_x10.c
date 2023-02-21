724b56d0f3a33188341fe95f054a5ee16dd85025
ShawenHarker <harkershawen@gmail.com>
2023-02-21 19:53:04 +0200

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Print the alphabet 10 times
+ *
+ * Return: Nothing
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int alpha;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
