4fc29fc5c7239f615e38b6813dc8812d4e2bf65b
ShawenHarker <harkershawen@gmail.com>
2023-02-21 12:26:17 +0200

    Task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,19 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
-*/
-int main()
+ */
+void print_alphabet(void)
 {
-    print_alphabet();
-    return (0);
+    int alpha;
+
+    for (alpha = 'a'; alpha <= 'z'; alpha++)
+    {
+        putchar(alpha);
+    }
+    putchar('\n');
+    return;
 }
---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,12 @@
+#include "main.c"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main()
+{
+    print_alphabet();
+    return (0);
+}
