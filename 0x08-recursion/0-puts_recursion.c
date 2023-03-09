021ae3f3fe7c358c39eccb6339b447d646e8f26b
ShawenHarker <harkershawen@gmail.com>
2023-03-09 17:17:35 +0200

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Print a string
+ * @s: The string.
+ * Return: Noting.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
